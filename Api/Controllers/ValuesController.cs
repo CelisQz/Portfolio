7142cd29a109b9fe41a87bef3e87e868ac6269c1
Celis <CelisCarlosQz@gmail.com>
2020-06-24 23:09:22 -0500

    Setting Up The Environment

---
+++ Api/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult IsWorking()
+        {
+            return Ok("Working");
+        }
+    }
+}
